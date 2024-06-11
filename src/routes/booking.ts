0d355497939e0035ee87c5f8cc4e8f868006a1e9
Bearkern <haolin1224@gmail.com>
2024-06-11 17:17:11 +0800

    feat: add booking
    1. GET: get all bookings
    2. POST: create booking

--- src/routes/booking.ts
+++ src/routes/booking.ts
@@ -1,7 +1,50 @@
 import { Router, Request, Response } from 'express';
 import Booking from '../model/bookingModel';
-import { ObjectId } from 'mongoose';
+import { Types } from 'mongoose';
 
 const router = Router();
+
+type BookingData = {
+  _id: Types.ObjectId,
+  package: string,
+  name: string,
+  gender: string,
+  babyGender: string,
+  address: string,
+  phone: string,
+  email: string,
+  date: string,
+  time: string,
+  availableTime: string,
+  availableWeek: string,
+  remark: string,
+}
+
+router.get('/', async (_, res: Response) => {
+  const bookings = await Booking.find();
+
+  res.send(bookings);
+});
+
+router.post('/', async (req: Request, res: Response) => {
+  const data = req.body;
+
+  const booking = await Booking.create({
+    photoPackage: data.photoPackage,
+    name: data.name,
+    gender: data.gender,
+    babyGender: data.babyGender,
+    address: data.address,
+    phone: data.phone,
+    email: data.email,
+    date: data.date,
+    time: data.time,
+    availableTime: data.availableTime,
+    availableWeek: data.availableWeek,
+    remark: data.remark,
+  });
+
+  res.send(booking);
+});
 
 export default router;
